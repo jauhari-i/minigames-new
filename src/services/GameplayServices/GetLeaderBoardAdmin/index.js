892545587d05dc104aec4c2b7fad452222a176f2
Irfan Jauhari <kangrykong123@gmail.com>
2021-03-03 16:14:39 +0700

    Im out

--- src/services/GameplayServices/GetLeaderBoardAdmin/index.js
+++ src/services/GameplayServices/GetLeaderBoardAdmin/index.js
@@ -7,74 +7,83 @@ const GetLeaderboardUser = async sort => {
   try {
     const lb = await Leaderboard.find()
 
-    const lbData = await Promise.all(
-      lb.map(async item => {
-        const game = await Game.findOne({ gameId: item.gameId })
-        if (!game) {
-          return null
-        } else {
-          const userGame = await MyGames.findOne({ myGameId: item.myGameId })
+    if (lb.length) {
+      const lbData = await Promise.all(
+        lb.map(async item => {
+          const game = await Game.findOne({ gameId: item.gameId })
+          if (!game) {
+            return null
+          } else {
+            const userGame = await MyGames.findOne({ myGameId: item.myGameId })
 
-          const user = await Users.findOne({ userId: userGame.userId })
+            const user = await Users.findOne({ userId: userGame.userId })
 
-          return {
-            leaderboardId: item.leaderboardId,
-            leaderName: item.teamLeaderName,
-            teamName: item.teamName,
-            teamIcon: item.teamIcon,
-            gameId: game.gameId,
-            gameData: {
+            return {
+              leaderboardId: item.leaderboardId,
+              leaderName: item.teamLeaderName,
+              teamName: item.teamName,
+              teamIcon: item.teamIcon,
               gameId: game.gameId,
-              gameTitle: game.gameTitle,
-              posterImage: game.posterImage.secure_url,
-              gameImage: game.gameImage.secure_url,
-              gameDescription: game.gameDescription,
-              gamePrice: game.gamePrice,
-              gameDiscount: game.gameDiscount,
-              gamePriceAfterDiscount: game.gamePriceAfterDiscount,
-              gameDifficulty: game.gameDifficulty,
-              gameRating: game.gameRating,
-              gameGenre: game.gameGenre,
-              gameDuration: game.gameDuration,
-              gameUrl: game.gameUrl,
-              gameCapacity: game.gameCapacity,
-              gameReady: game.gameReady,
-              createdAt: game.createdAt,
-              createdBy: game.createdBy,
-            },
-            userId: user.userId,
-            score: item.score,
-            createdAt: item.createdAt,
+              gameData: {
+                gameId: game.gameId,
+                gameTitle: game.gameTitle,
+                posterImage: game.posterImage.secure_url,
+                gameImage: game.gameImage.secure_url,
+                gameDescription: game.gameDescription,
+                gamePrice: game.gamePrice,
+                gameDiscount: game.gameDiscount,
+                gamePriceAfterDiscount: game.gamePriceAfterDiscount,
+                gameDifficulty: game.gameDifficulty,
+                gameRating: game.gameRating,
+                gameGenre: game.gameGenre,
+                gameDuration: game.gameDuration,
+                gameUrl: game.gameUrl,
+                gameCapacity: game.gameCapacity,
+                gameReady: game.gameReady,
+                createdAt: game.createdAt,
+                createdBy: game.createdBy,
+              },
+              userId: user.userId,
+              score: item.score,
+              createdAt: item.createdAt,
+            }
           }
-        }
+        })
+      )
+
+      const data = lbData.filter(el => {
+        return el != null
       })
-    )
 
-    const data = lbData.filter(el => {
-      return el != null
-    })
+      let sorted
 
-    let sorted
+      if (sort === 'score') {
+        sorted = data.sort((a, b) => {
+          if (a.score < b.score) return -1
+          if (a.score > b.score) return 1
+          return 0
+        })
+      } else {
+        sorted = data.sort((a, b) => {
+          if (new Date(a.createdAt) < new Date(b.createdAt)) return -1
+          if (new Date(a.createdAt) > new Date(b.createdAt)) return 1
+          return 0
+        })
+      }
 
-    if (sort === 'score') {
-      sorted = data.sort((a, b) => {
-        if (a.score < b.score) return -1
-        if (a.score > b.score) return 1
-        return 0
-      })
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Get leaderboard success',
+        data: sorted,
+      }
     } else {
-      sorted = data.sort((a, b) => {
-        if (new Date(a.createdAt) < new Date(b.createdAt)) return -1
-        if (new Date(a.createdAt) > new Date(b.createdAt)) return 1
-        return 0
-      })
-    }
-
-    return {
-      success: true,
-      statusCode: 200,
-      message: 'Get leaderboard success',
-      data: sorted,
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Get leaderboard success',
+        data: [],
+      }
     }
   } catch (error) {
     return error
