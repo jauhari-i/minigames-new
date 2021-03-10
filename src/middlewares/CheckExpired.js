e573bf99408970967af94b765690d406267e4960
Irfan Jauhari <kangrykong123@gmail.com>
2021-03-10 21:55:02 +0700

    Fixed expiration

--- src/middlewares/CheckExpired.js
+++ src/middlewares/CheckExpired.js
@@ -2,7 +2,7 @@ export const checkExpired = (playDate, timeEnd) => {
   const today = new Date()
   const playingDate = new Date(playDate)
 
-  if (playingDate.setHours(timeEnd, 0, 0, 0) <= today.setHours(0, 0, 0, 0)) {
+  if (playingDate.setHours(timeEnd, 0, 0, 0) <= today.setHours(23, 59, 0, 0)) {
     return true
   }
 
