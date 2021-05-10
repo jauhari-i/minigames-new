e8d06b5fbb8f1d061951df528d4bec40cec4de86
telkomdev-irfan_jauhari <kangrykong123@gmail.com>
2021-05-10 16:00:07 +0700

    Add hours

--- src/constants/timePlay.js
+++ src/constants/timePlay.js
@@ -1,4 +1,20 @@
-export const timeArray = [7, 9, 12, 14, 16, 19, 21]
+export const timeArray = [
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+]
 
 export const getTimeStart = t => {
   const tIndex = t - 1
